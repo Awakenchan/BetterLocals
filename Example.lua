3df8ff24e745a95e708e883fcaeac50b46beb00b
Awaken <93601997+Awakenchan@users.noreply.github.com>
2025-11-22 03:31:13 +0500

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -2,4 +2,4 @@ local BetterLocals = loadstring(game:HttpGet("https://raw.githubusercontent.com/
 repeat task.wait() until RecursiveTable ~= nil
 local ReplicatedStorageData = RecursiveTable(ReplicatedStorage)
 local Class,Default = loadstring(game:HttpGet("https://raw.githubusercontent.com/Awakenchan/GcViewerV2/refs/heads/main/Utility/Data2Code%40Amity.lua"))()
-print(Class.Convert(ReplicatedStorageData,true), " other usage >"CharacterPart.Head().Position,LocalPlayer():GetFullName())
+print(Class.Convert(ReplicatedStorageData,true), " other usage >",CharacterPart.Head().Position,LocalPlayer():GetFullName())
